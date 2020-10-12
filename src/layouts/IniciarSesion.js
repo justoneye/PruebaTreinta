18852d2d931af937d306c73bceaf49f925b784df
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2020-10-12 12:56:48 -0500

    Indentación corregida

--- src/layouts/IniciarSesion.js
+++ src/layouts/IniciarSesion.js
@@ -15,59 +15,57 @@ class IniciarSesion extends Component {
                 </span>
 
                 <div class="wrap-input100 validate-input" data-validate="Este campo es OBLIGATORIO">
-                <span class="label-input100">Nombre Completo</span>
+                  <span class="label-input100">Nombre Completo</span>
                     <input class="input100" type="text" name="name" placeholder="Nombre"/>
-                <span class="focus-input100"></span>
+                  <span class="focus-input100"></span>
                 </div>
 
                 <div class="wrap-input100 validate-input" data-validate = "Este campo es OBLIGATORIO">
-                <span class="label-input100">Email</span>
+                  <span class="label-input100">Email</span>
                     <input class="input100" type="text" name="email" placeholder="Email"/>
-                <span class="focus-input100"></span>
+                  <span class="focus-input100"></span>
                 </div>
 
                 <div class="wrap-input100 validate-input" data-validate = "Este campo es OBLIGATORIO">
-                <span class="label-input100">Contraseña</span>
+                  <span class="label-input100">Contraseña</span>
                     <input class="input100" type="text" name="pass" placeholder="*************"/>
-                <span class="focus-input100"></span>
+                  <span class="focus-input100"></span>
                 </div>
 
                 <div class="wrap-input100 validate-input" data-validate = "Este campo es OBLIGATORIO">
-                <span class="label-input100">Repeat Password</span>
+                  <span class="label-input100">Repeat Password</span>
                     <input class="input100" type="text" name="repeat-pass" placeholder="*************"/>
-                <span class="focus-input100"></span>
+                  <span class="focus-input100"></span>
                 </div>
 
                 <div class="flex-m w-full p-b-33">
-                <div class="contact100-form-checkbox">
+                  <div class="contact100-form-checkbox">
                     <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me"/>
-                    <label class="label-checkbox100" for="ckb1">
+                      <label class="label-checkbox100" for="ckb1">
                         <span class="txt1"> 
-                    Estoy de acuerdo con los <a href="#" class="txt2 hov1">Términos y Políticas de Uso</a>
-                    </span>
-                    </label>
-                </div>
+                        Estoy de acuerdo con los <a href="#" class="txt2 hov1">Términos y Políticas de Uso</a>
+                        </span>
+                      </label>
+                  </div>
 
-            <div class="container-login100-form-btn">
-                <div class="wrap-login100-form-btn">
-                <div class="login100-form-bgbtn">
-
-                </div>
+                  <div class="container-login100-form-btn">
+                    <div class="wrap-login100-form-btn">
+                      <div class="login100-form-bgbtn">
+                      </div>
                     <button class="login100-form-btn">
-                    Sign Up
+                      Sign Up
                     </button>
                 </div>
 
                 <a href="#" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
-                    Sign in
-                    <i class="fa fa-long-arrow-right m-l-5"></i>
+                  Sign in <i class="fa fa-long-arrow-right m-l-5"></i>
                 </a>
-			</div>
-          </div>
-		</form>
-		</div>
-	</div>
-    </div>
+			    </div>
+              </div>
+		    </form>
+		  </div>
+	    </div>
+      </div>
     );
   }
 };
