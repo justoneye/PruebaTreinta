a64d941c029a1cfc315733d7dedac6aaf0d795e4
Pilar Camargo Márquez <ing.pilarcamargo@gmail.com>
2020-10-12 12:37:30 -0500

    Cambio en los ítems de inicio

--- src/layouts/IniciarSesion.js
+++ src/layouts/IniciarSesion.js
@@ -9,36 +9,30 @@ class IniciarSesion extends Component {
         <div class="container-login100">
           <div class="login100-more" style="background-image: url('images/bg-01.jpg');"></div>
             <div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-            <form class="login100-form validate-form">
-              <span class="login100-form-title p-b-59">
-                Sign Up
-              </span>
+              <form class="login100-form validate-form">
+                <span class="login100-form-title p-b-59">
+                  Registro
+                </span>
 
-                <div class="wrap-input100 validate-input" data-validate="Name is required">
-                <span class="label-input100">Full Name</span>
-                    <input class="input100" type="text" name="name" placeholder="Name..."/>
+                <div class="wrap-input100 validate-input" data-validate="Este campo es OBLIGATORIO">
+                <span class="label-input100">Nombre Completo</span>
+                    <input class="input100" type="text" name="name" placeholder="Nombre"/>
                 <span class="focus-input100"></span>
                 </div>
 
-                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                <div class="wrap-input100 validate-input" data-validate = "Este campo es OBLIGATORIO">
                 <span class="label-input100">Email</span>
-                    <input class="input100" type="text" name="email" placeholder="Email addess..."/>
+                    <input class="input100" type="text" name="email" placeholder="Email"/>
                 <span class="focus-input100"></span>
                 </div>
 
-                <div class="wrap-input100 validate-input" data-validate="Username is required">
-                <span class="label-input100">Username</span>
-                    <input class="input100" type="text" name="username" placeholder="Username..."/>
-                <span class="focus-input100"></span>
-                </div>
-
-                <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                <span class="label-input100">Password</span>
+                <div class="wrap-input100 validate-input" data-validate = "Este campo es OBLIGATORIO">
+                <span class="label-input100">Contraseña</span>
                     <input class="input100" type="text" name="pass" placeholder="*************"/>
                 <span class="focus-input100"></span>
                 </div>
 
-                <div class="wrap-input100 validate-input" data-validate = "Repeat Password is required">
+                <div class="wrap-input100 validate-input" data-validate = "Este campo es OBLIGATORIO">
                 <span class="label-input100">Repeat Password</span>
                     <input class="input100" type="text" name="repeat-pass" placeholder="*************"/>
                 <span class="focus-input100"></span>
@@ -49,7 +43,7 @@ class IniciarSesion extends Component {
                     <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me"/>
                     <label class="label-checkbox100" for="ckb1">
                         <span class="txt1"> 
-                    I agree to the <a href="#" class="txt2 hov1">Terms of User</a>
+                    Estoy de acuerdo con los <a href="#" class="txt2 hov1">Términos y Políticas de Uso</a>
                     </span>
                     </label>
                 </div>
